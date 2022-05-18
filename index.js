f8366847b17d0fefb44f93a7d3414c0fb81eb863
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-18 23:04:44 +0600

    setup complete on backend

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wcxgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('toDoApp').collection('tasks')
 
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
